5c0bde4b1f8ae698fac01f6897417b762d044c58
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-06-09 16:01:44 +0300

    Add files via upload

--- Finalproject/blog/urls.py
+++ Finalproject/blog/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from blog.views import index, create
+from blog.views import index, create, detail
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', index, name = "home" ),
-    path("create/", create, name = "create-page")
-]
-
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("create/", create, name = "create-page"),
+    path("posts/<int:post_id>/", detail),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Finalproject/blog/views.py
+++ Finalproject/blog/views.py
@@ -1,17 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from blog.models import Post
+
+
 # Create your views here.
+def handle_uploaded_file(f):
+    with open("media/" + f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def index(request):
-    posts = Post.objects.all()
-    dictionary = {"posts" : posts}
-    return render(request, "index.html", dictionary)
+        posts = Post.objects.all()
+        dictionary = {"posts" : posts}
+        return render(request, "index.html", dictionary)
 
 def create(request):
     if request.method == "POST":
-        obj = Post(title = request.POST.get("title"),
+        print(request.FILES)
+        object1 = Post(title = request.POST.get("title"),
                     text = request.POST.get("text"),
-                    image = request.POST.get("image"))
-        obj.save()
+                    image = request.FILES.get("image").name)
+        object1.save()
+        handle_uploaded_file(request.FILES["image"])
         return render(request, "success.html")
     return render(request, "create.html")
+
+
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk = post_id)
+    context = {"post": post}
+    return render(request, "details.html", context)
