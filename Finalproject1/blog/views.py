a1e9d04e293ff350f828e1e8768324a6b20c1292
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-06-15 09:26:28 +0300

    Add files via upload

---
+++ Finalproject1/blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from blog.models import Post
+
+
+# Create your views here.
+def handle_uploaded_file(f):
+    with open("media/" + f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
+def index(request):
+        posts = Post.objects.all()
+        dictionary = {"posts" : posts}
+        return render(request, "index.html", dictionary)
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        print(request.FILES)
+        object1 = Post(title = request.POST.get("title"),
+                    text = request.POST.get("text"),
+                    image = request.FILES.get("image").name,
+                    author = request.user)
+        object1.save()
+        handle_uploaded_file(request.FILES["image"])
+        return render(request, "success.html")
+    return render(request, "create.html")
+
+
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk = post_id)
+    context = {"post": post}
+    return render(request, "details.html", context)
